03e9ad3f2ae5795c8747bf6f8d92fcdb53fce439
Danny Purcell <dpurcelljr@gmail.com>
2013-05-06 00:32:26 -0500

    Using yard for method docs

--- src/main/scripts/Test.rb
+++ src/main/scripts/Test.rb
@@ -6,8 +6,22 @@ module Test
   # A method that tells us what kitties say
   #
   # @param [String] say what kitties should say
+  # @param [Integer] count how many times kitties say it
   def self.kitties(say='MAOW!',count)
     puts "Kitties: #{say.count.times}"
+  end
+
+  # A method that tests a code value
+  #
+  # @param [Integer] test_code the code value to test
+  # @return a return value
+  def self.test(test_code)
+    case test_code
+      when 1
+        return 0
+      else
+        return 1
+    end
   end
 
   # A test method for other stuff
--- src/main/scripts/rubycom.rb
+++ src/main/scripts/rubycom.rb
@@ -2,30 +2,50 @@ require 'yard'
 module Rubycom
 
   def self.included(base)
-
-    base_singleton_methods = base.singleton_methods(false)
     puts "BaseClass: #{base}"
     puts "Public/ProtectedStaticMethods:"
+    puts Rubycom.get_usage(base,true)
+  end
+
+  def self.get_usage(base, print_extended=false)
+    return_str = ""
+    base_singleton_methods = base.singleton_methods(false)
     base_singleton_methods.each { |sym|
       m = base.public_method(sym)
+      method_doc = self.get_doc(m)
       req_params=[]
       optional_params=[]
       m.parameters.each { |opt_req, symbol|
         req_params<<symbol if opt_req==:req
         optional_params<<symbol if opt_req==:opt
       }
-      msg = "Usage: #{m.name} [option]"
+      msg = "#{m.name} -\t#{method_doc[:desc]}\n"
+      extended_msg = "Usage: #{m.name}"
       req_params.each { |param|
-        msg << " #{param}"
+        extended_msg << " #{param}"
       }
-      msg<<"\n"
-      optional_params.each { |option|
-        msg<<"\t -#{option}\n"
+      extended_msg << " ["
+      optional_params.each_with_index { |option,index|
+        if index == 0
+          extended_msg << "#{option}"
+        else
+          extended_msg << "|#{option}"
+        end
       }
-      msg << "Source: #{m.name}\n"
-      msg << self.get_doc(m)
-      puts msg
+      extended_msg << "]\n"
+      extended_msg << "Parameters:\n"
+      method_doc[:params].each{ |param_doc|
+        extended_msg << "\t#{param_doc.gsub("[","").gsub("]"," -")}\n"
+      }
+      extended_msg << "Returns: #{method_doc[:return]}\n"
+      if print_extended
+        return_str << "#{msg}"
+        return_str << "#{extended_msg}\n"
+      else
+        return_str << msg
+      end
     }
+    return_str
   end
 
   def self.get_doc(method)
@@ -33,33 +53,42 @@ module Rubycom
     doc_str = ""
     YARD.parse_string(File.read(source_file)).enumerator.each{|sexp|
       method_hash = Rubycom.retrieve_method_hash(sexp,method)
-      doc_str = method_hash.to_s#method_hash[:doc] || "nil"
+      doc_str = method_hash[:method_doc] || "nil"
     }
-    doc_str
+    doc_hash = {}
+    doc_str.split("\n").each{|doc_line|
+      if doc_line.include?"@param"
+        param_doc = doc_line.gsub("@param","").lstrip
+        params = doc_hash[:params]
+        if params.nil? || params.length == 0
+          params = [param_doc]
+        else
+          params << param_doc
+        end
+        doc_hash[:params] = params
+      elsif doc_line.include?"@return"
+        doc_hash[:return] = doc_line.gsub("@return","")
+      else
+        doc_hash[:desc] = doc_line unless doc_line.lstrip.length==0
+      end
+    }
+    if doc_hash[:return].nil?
+      doc_hash[:return] = "void"
+    end
+    doc_hash
   end
 
   def self.retrieve_method_hash(sexp_arr, method)
     return {} if (sexp_arr.nil? || sexp_arr.length == 0)
-    result_list = []
+    result_hash = {}
     sexp_arr.each{|sexp|
-      puts "#{sexp.docstring}"
-      if (sexp.children[2].source.include? "#{method.name}") && (sexp.type == :defs)
-        params_hash = {}
-        sexp.jump(:params).children.each{ |param_sexp|
-
-        }
-        result_list << { method_desc:sexp.docstring, param_desc:}
+      if (sexp.type == :defs) && (sexp.children[2].source.include? "#{method.name}")
+        result_hash = { method_doc:sexp.docstring, method_sexp: sexp }
       else
-        result_list << Rubycom.retrieve_method_hash(sexp.children,method)
+        result_hash = Rubycom.retrieve_method_hash(sexp.children,method) if result_hash.length == 0
       end
     }
-    return_list = []
-    result_list.each{ |result|
-      unless result.nil? || result.length == 0
-        return_list << result
-      end
-    }
-    return_list
+    result_hash
   end
 
 end
