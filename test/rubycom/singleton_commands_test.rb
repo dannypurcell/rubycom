0973612fffd2b30b1be81fc0cf1dc95e5d8e6071
Danny Purcell <dpurcelljr@gmail.com>
2013-10-08 08:32:30 -0500

    Sets up sources module

--- test/rubycom/singleton_commands_test.rb
+++ test/rubycom/singleton_commands_test.rb
@@ -11,48 +11,68 @@ class SingletonCommandsTest < Test::Unit::TestCase
   def test_discover_commands
     test_mod = UtilTestComposite
     result = Rubycom::SingletonCommands.discover_commands(test_mod)
-    expected = {:UtilTestComposite =>
-                    {:UtilTestModule => {:type => :module},
-                     :UtilTestNoSingleton => {:type => :module},
-                     :test_composite_command => {:type => :command}}}
+    expected = {:UtilTestComposite=>
+                    {:commands=>
+                         {:UtilTestModule=>
+                              {:commands=>
+                                   {:test_command=>{:type=>:command},
+                                    :test_command_all_options=>{:type=>:command},
+                                    :test_command_arg_arr=>{:type=>:command},
+                                    :test_command_arg_false=>{:type=>:command},
+                                    :test_command_arg_hash=>{:type=>:command},
+                                    :test_command_arg_named_arg=>{:type=>:command},
+                                    :test_command_arg_timestamp=>{:type=>:command},
+                                    :test_command_mixed_options=>{:type=>:command},
+                                    :test_command_no_docs=>{:type=>:command},
+                                    :test_command_options_arr=>{:type=>:command},
+                                    :test_command_with_arg=>{:type=>:command},
+                                    :test_command_with_args=>{:type=>:command},
+                                    :test_command_with_options=>{:type=>:command},
+                                    :test_command_with_return=>{:type=>:command}},
+                               :type=>:module},
+                          :UtilTestNoSingleton=>{:commands=>{}, :type=>:module},
+                          :test_composite_command=>{:type=>:command}},
+                     :type=>:module}}
     assert_equal(expected, result)
   end
 
   def test_get_top_level_commands
     test_mod = UtilTestComposite
     result = Rubycom::SingletonCommands.get_top_level_commands(test_mod)
-    expected = {:UtilTestComposite =>
-                    {:UtilTestModule => {:type => :module},
-                     :UtilTestNoSingleton => {:type => :module},
-                     :test_composite_command => {:type => :command}}}
+    expected = {:UtilTestComposite=>
+                    {:commands=>
+                         {:UtilTestModule=>{:type=>:module},
+                          :UtilTestNoSingleton=>{:type=>:module},
+                          :test_composite_command=>{:type=>:command}},
+                     :type=>:module}}
     assert_equal(expected, result)
   end
 
   def test_get_commands
     test_mod = UtilTestComposite
     result = Rubycom::SingletonCommands.get_commands(test_mod)
-    expected = {:UtilTestComposite =>
-                    {:test_composite_command => {:type => :command},
-                     UtilTestNoSingleton =>
-                         {:commands => {:UtilTestNoSingleton => {}}, :type => :module},
-                     UtilTestModule =>
-                         {:commands =>
-                              {:UtilTestModule =>
-                                   {:test_command => {:type => :command},
-                                    :test_command_all_options => {:type => :command},
-                                    :test_command_arg_arr => {:type => :command},
-                                    :test_command_arg_false => {:type => :command},
-                                    :test_command_arg_hash => {:type => :command},
-                                    :test_command_arg_named_arg => {:type => :command},
-                                    :test_command_arg_timestamp => {:type => :command},
-                                    :test_command_mixed_options => {:type => :command},
-                                    :test_command_no_docs => {:type => :command},
-                                    :test_command_options_arr => {:type => :command},
-                                    :test_command_with_arg => {:type => :command},
-                                    :test_command_with_args => {:type => :command},
-                                    :test_command_with_options => {:type => :command},
-                                    :test_command_with_return => {:type => :command}}},
-                          :type => :module}}}
+    expected = {:UtilTestComposite=>
+                    {:commands=>
+                         {:UtilTestModule=>
+                              {:commands=>
+                                   {:test_command=>{:type=>:command},
+                                    :test_command_all_options=>{:type=>:command},
+                                    :test_command_arg_arr=>{:type=>:command},
+                                    :test_command_arg_false=>{:type=>:command},
+                                    :test_command_arg_hash=>{:type=>:command},
+                                    :test_command_arg_named_arg=>{:type=>:command},
+                                    :test_command_arg_timestamp=>{:type=>:command},
+                                    :test_command_mixed_options=>{:type=>:command},
+                                    :test_command_no_docs=>{:type=>:command},
+                                    :test_command_options_arr=>{:type=>:command},
+                                    :test_command_with_arg=>{:type=>:command},
+                                    :test_command_with_args=>{:type=>:command},
+                                    :test_command_with_options=>{:type=>:command},
+                                    :test_command_with_return=>{:type=>:command}},
+                               :type=>:module},
+                          :UtilTestNoSingleton=>{:commands=>{}, :type=>:module},
+                          :test_composite_command=>{:type=>:command}},
+                     :type=>:module}}
     assert_equal(expected, result)
   end
 
@@ -60,9 +80,11 @@ class SingletonCommandsTest < Test::Unit::TestCase
     test_mod = UtilTestComposite
     result = Rubycom::SingletonCommands.get_commands(test_mod, false)
     expected = {:UtilTestComposite=>
-                    {:test_composite_command=>{:type=>:command},
-                     UtilTestNoSingleton=>{:type=>:module},
-                     UtilTestModule=>{:type=>:module}}}
+                    {:commands=>
+                         {:UtilTestModule=>{:type=>:module},
+                          :UtilTestNoSingleton=>{:type=>:module},
+                          :test_composite_command=>{:type=>:command}},
+                     :type=>:module}}
     assert_equal(expected, result)
   end
 
